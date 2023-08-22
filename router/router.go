bf0da5cd62c63f33df2721ab284900f61ee747c7
wsldev <wslmacieira@gmail.com>
2023-08-22 13:24:37 -0300

    feat: update handler structure to separate package

--- router/router.go
+++ router/router.go
@@ -3,14 +3,10 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	// Iniciando o Router utilizando as configurações Default do gin
+	// Initialize Router
 	router := gin.Default()
-	// Definindo uma rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// Estamos rodando a nossa api
+	// Initialize Routes
+	initializeRoutes(router)
+	// Run the server
 	router.Run(":8080")
 }
