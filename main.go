4269b780d034d6b61ad89ee4727f2653e9e64089
wsldev <wslmacieira@gmail.com>
2023-08-22 23:09:53 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/wsldev/gopportunities/router"
+import (
+	"github.com/wsldev/gopportunities/config"
+	"github.com/wsldev/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize Router
 	router.Initialize()
 }
