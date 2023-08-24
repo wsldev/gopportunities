e0dad0f785c971732f521ba9642bbb3567e34acf
wsldev <wslmacieira@gmail.com>
2023-08-23 22:39:51 -0300

    feat: add updateOpening handler logic

--- handler/request.go
+++ handler/request.go
@@ -6,7 +6,7 @@ func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 }
 
-// CretaeOpening
+// CreateOpening
 
 type CreateOpeningRequest struct {
 	Role     string `json:"role"`
@@ -18,7 +18,7 @@ type CreateOpeningRequest struct {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
-	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Link == "" && r.Salary <= 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
 	if r.Role == "" {
@@ -40,4 +40,24 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "uint64")
 	}
 	return nil
+}
+
+// UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// if none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provider")
 }
